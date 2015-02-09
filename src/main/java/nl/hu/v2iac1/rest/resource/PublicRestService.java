33f22521472b65d7e7db5c8924e7818e595b73f7
Roelant Ossewaarde <roelanto@gmail.com>
2015-02-09 01:28:53 -0100

    Addition of configuration; migrated to jersey 2x

--- src/main/java/nl/hu/v2iac1/rest/resource/PublicRestService.java
+++ src/main/java/nl/hu/v2iac1/rest/resource/PublicRestService.java
@@ -1,17 +1,19 @@
-package nl.hu.v2iac1;
+package nl.hu.v2iac1.rest.resource;
+
+import nl.hu.v2iac1.Configuration;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
 @Path("/public")
-public class PublicRestService {
+public class PublicRestService extends AbstractRestService {
 
     @GET
     @Path("/")
     public Response getSecret() {
 
-        String output = "This is public!";
+        String output = "This is public: "+ configuration.getValue(Configuration.Key.PUBLIC);
         return Response.status(200).entity(output).build();
 
     }
