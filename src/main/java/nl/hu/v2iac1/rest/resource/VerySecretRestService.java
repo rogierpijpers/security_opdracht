a85208f8c09e95345d0049dc306872de11882ad4
Hans <hansvanos91@hotmail.com>
2016-02-22 16:47:02 +0100

    Zo wat opgemaakt

--- src/main/java/nl/hu/v2iac1/rest/resource/VerySecretRestService.java
+++ src/main/java/nl/hu/v2iac1/rest/resource/VerySecretRestService.java
@@ -1,25 +1,16 @@
 package nl.hu.v2iac1.rest.resource;
 
 import nl.hu.v2iac1.Configuration;
-
 import java.net.URI;
 import java.net.URISyntaxException;
-
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
-
-import com.google.api.client.auth.oauth2.AuthorizationCodeResponseUrl;
 
 @Path("/verysecret")
 @Produces(MediaType.TEXT_PLAIN)
@@ -30,19 +21,15 @@ public class VerySecretRestService extends AbstractRestService {
 
 	@GET
 	@Path("/{token}")
-	public Response getSecret(@Context UriInfo uriInfo, @Context Response resp, @PathParam("token") String token)
-			throws URISyntaxException {
-		String link = uriInfo.getRequestUri().toString();
+	public Response getSecret(@PathParam("token") String token) throws URISyntaxException {
+
 		URI uri = new URI("http://localhost:8080/rest/verysecret/authenticate");
-		
-		System.out.println(link);
-		
+
 		if (!token.equals(clientsecret)) {
-			
+
 			return Response.temporaryRedirect(uri).build();
 		}
-		String output = "This is very secret: "
-				+ configuration.getValue(Configuration.Key.VERYSECRET);
+		String output = "This is very secret: " + configuration.getValue(Configuration.Key.VERYSECRET);
 		return Response.status(200).entity(output).build();
 
 	}
@@ -53,14 +40,11 @@ public class VerySecretRestService extends AbstractRestService {
 	public Response authenticate() {
 		URI uri = null;
 		try {
-			uri = new URI(
-					"https://accounts.google.com/o/oauth2/v2/auth?"
-							+ "client_id=670282150821-0kd0pjsm09re6mtvjdm5s9ejek3hc4a0.apps.googleusercontent.com&"
-							+ "response_type=code"
-							+ "&scope=openid%20email&"
-							+ "state=http://localhost:8080&"
-							+ "acces_type=online&"
-							+ "redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Frest%2Fverysecret%2Fauthorize");
+			uri = new URI("https://accounts.google.com/o/oauth2/v2/auth?"
+					+ "client_id=670282150821-0kd0pjsm09re6mtvjdm5s9ejek3hc4a0.apps.googleusercontent.com&"
+					+ "response_type=code" + "&scope=openid%20email&" + "state=http://localhost:8080&"
+					+ "acces_type=online&"
+					+ "redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Frest%2Fverysecret%2Fauthorize");
 		} catch (URISyntaxException e) {
 
 			e.printStackTrace();
@@ -71,9 +55,7 @@ public class VerySecretRestService extends AbstractRestService {
 
 	@GET
 	@Path("/authorize")
-	public Response authorize(@QueryParam("code") String code,
-			@QueryParam("state") String state, @Context Request request,
-			@Context UriInfo uriInfo) throws URISyntaxException {
+	public Response authorize(@Context UriInfo uriInfo) throws URISyntaxException {
 
 		URI uri = new URI("http://localhost:8080");
 		String link = uriInfo.getRequestUri().toString();
@@ -84,34 +66,8 @@ public class VerySecretRestService extends AbstractRestService {
 			return Response.temporaryRedirect(uri).build();
 		}
 
-		// request token
-//
-//		 try {
-//		 uri = new URI("https://www.googleapis.com/oauth2/v4/token?"+
-//		 "code=4/EDxem0UwFLAT2xY8eMeO9nmNO9z-f6rD7zlkxAj5g3o"+
-//		 "client_id=670282150821-0kd0pjsm09re6mtvjdm5s9ejek3hc4a0.apps.googleusercontent.com"+
-//		"client-secret=wEWpecZ0wi2Npw44WS8xceof"+
-//		 "redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Frest%2Fverysecret"+
-//		 "grant_type=authorization_code");
-//		
-//		 } catch (URISyntaxException e) {
-//		// TODO Auto-generated catch block
-//		 e.printStackTrace();
-//		 }
-//		//
-//		//
-//		 System.out.println(Response.temporaryRedirect(uri).build());
-		// return Response.temporaryRedirect(uri).build();
-		uri = new URI("http://localhost:8080/rest/verysecret/"+clientsecret);
+		uri = new URI("http://localhost:8080/rest/verysecret/" + clientsecret);
 		return Response.temporaryRedirect(uri).build();
 	}
 
-	/*
-	 * https://accounts.google.com/o/oauth2/v2/auth?client_id=670282150821-0
-	 * kd0pjsm09re6mtvjdm5s9ejek3hc4a0
-	 * .apps.googleusercontent.com&response_type=code&scope=openid%
-	 * 20email&state=http://localhost:8080/rest/verysecret&acces_type=offline&redirect_uri=http%3A%2F%2Flocalhost:8080%2Frest%2Fverysecret
-	 * 
-	 * code=4/EDxem0UwFLAT2xY8eMeO9nmNO9z-f6rD7zlkxAj5g3o
-	 */
 }
