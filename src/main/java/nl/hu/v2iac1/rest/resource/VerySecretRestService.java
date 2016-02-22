de51c6ef43bf38898a803f7d845373f2d264bb6d
Hans <hansvanos91@hotmail.com>
2016-02-22 14:41:33 +0100

    Ziet eruit alsof het werkt. werkt niet :D

--- src/main/java/nl/hu/v2iac1/rest/resource/VerySecretRestService.java
+++ src/main/java/nl/hu/v2iac1/rest/resource/VerySecretRestService.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import com.google.api.client.auth.oauth2.AuthorizationCodeResponseUrl;
@@ -40,7 +41,12 @@ public class VerySecretRestService extends AbstractRestService {
     public Response authenticate(){
     	URI uri = null;
 		try {
-			uri = new URI("https://accounts.google.com/o/oauth2/v2/auth?client_id=670282150821-0kd0pjsm09re6mtvjdm5s9ejek3hc4a0.apps.googleusercontent.com&response_type=code&scope=openid%20email&state=http://localhost:8080/rest/verysecret&acces_type=offline&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Frest%2Fauthorize");
+			uri = new URI("https://accounts.google.com/o/oauth2/v2/auth?"+
+		"client_id=670282150821-0kd0pjsm09re6mtvjdm5s9ejek3hc4a0.apps.googleusercontent.com&"+
+		"response_type=code&scope=openid%20email&"+
+		"state=http://localhost:8080/rest/verysecret&"+
+		"acces_type=online&"+
+		"redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Frest%2Fverysecret%2Fauthorize");
 		} catch (URISyntaxException e) {
 			 
 			e.printStackTrace();
@@ -53,14 +59,15 @@ public class VerySecretRestService extends AbstractRestService {
     }
     @GET
     @Path("/authorize")
-    public Response authorize(@QueryParam("code")String code,@QueryParam("state")String state,@Context UriInfo uriInfo){
-    	UriInfo sb = uriInfo;
+    public Response authorize(@QueryParam("code")String code,@QueryParam("state")String state,@Context Request request,@Context UriInfo uriInfo){
+    	
     	URI uri = null;
-		
+		String link = uriInfo.getPath().toString();
 		
     	
-    	AuthorizationCodeResponseUrl authResponse = new AuthorizationCodeResponseUrl(sb.toString());
-    	if(authResponse.getError() !=null){
+    	
+    	
+    	if(link.contains("error")){
     		//denied
     		try {
     			uri = new URI("http://localhost:8080");
@@ -74,9 +81,15 @@ public class VerySecretRestService extends AbstractRestService {
     	
     		//request token
 
-
+    	
     	try {
-			uri = new URI("https://accounts.google.com/o/oauth2/token?code=4/EDxem0UwFLAT2xY8eMeO9nmNO9z-f6rD7zlkxAj5g3o&client_id=670282150821-0kd0pjsm09re6mtvjdm5s9ejek3hc4a0.apps.googleusercontent.com&client_secret=wEWpecZ0wi2Npw44WS8xceof&redirect_uri=https%3A%2F%2Flocalhost%3A8080%2Frest%2Fversecret&grant_type=authorization_code");
+			uri = new URI("https://www.googleapis.com/oauth2/v4/token?"+
+    	"code=4/EDxem0UwFLAT2xY8eMeO9nmNO9z-f6rD7zlkxAj5g3o"+
+		"client_id=670282150821-0kd0pjsm09re6mtvjdm5s9ejek3hc4a0.apps.googleusercontent.com"+
+    	"client-secret=wEWpecZ0wi2Npw44WS8xceof"+
+		"redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Frest%2Fverysecret"+
+    	"grant_type=authorization_code");
+		
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -91,5 +104,8 @@ public class VerySecretRestService extends AbstractRestService {
      https://accounts.google.com/o/oauth2/v2/auth?client_id=670282150821-0kd0pjsm09re6mtvjdm5s9ejek3hc4a0.apps.googleusercontent.com&response_type=code&scope=openid%20email&state=http://localhost:8080/rest/verysecret&acces_type=offline&redirect_uri=http%3A%2F%2Flocalhost:8080%2Frest%2Fverysecret    	 
     	 
     code=4/EDxem0UwFLAT2xY8eMeO9nmNO9z-f6rD7zlkxAj5g3o
+    
+    
+    
     	 */
 }
