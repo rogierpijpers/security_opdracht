ce5aa3bddd367d26f57164ea56e4ce106c0dbb85
Hans <hansvanos91@hotmail.com>
2016-02-22 15:18:03 +0100

    Lekker veel gecomment

--- src/main/java/nl/hu/v2iac1/rest/resource/VerySecretRestService.java
+++ src/main/java/nl/hu/v2iac1/rest/resource/VerySecretRestService.java
@@ -24,13 +24,16 @@ public class VerySecretRestService extends AbstractRestService {
 	
 	String clientID = "670282150821-0kd0pjsm09re6mtvjdm5s9ejek3hc4a0.apps.googleusercontent.com";
 	String clientsecret = "wEWpecZ0wi2Npw44WS8xceof";
-	String scope = "https://www.googleapis.com/auth/drive.metadata.readonly";
-	String redirectURI = "http:localhost:8080/oauth2callback";
+
 	
     @GET
     @Path("/")
-    public Response getSecret() {
-
+    public Response getSecret(@Context UriInfo uriInfo) throws URISyntaxException {
+//    	String link = uriInfo.getRequestUri().toString();
+//    	URI uri = new URI("http://localhost:8080/rest/verysecret/authenticate");
+//    	if(!(link.equals("http://localhost:8080/rest/verysecret#"))){
+//    		return Response.temporaryRedirect(uri).build();
+//    	}
         String output = "This is very secret: " + configuration.getValue(Configuration.Key.VERYSECRET);
         return Response.status(200).entity(output).build();
 
@@ -43,8 +46,9 @@ public class VerySecretRestService extends AbstractRestService {
 		try {
 			uri = new URI("https://accounts.google.com/o/oauth2/v2/auth?"+
 		"client_id=670282150821-0kd0pjsm09re6mtvjdm5s9ejek3hc4a0.apps.googleusercontent.com&"+
-		"response_type=code&scope=openid%20email&"+
-		"state=http://localhost:8080/rest/verysecret&"+
+		"response_type=code"+
+		"&scope=openid%20email&"+
+		"state=http://localhost:8080&"+
 		"acces_type=online&"+
 		"redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Frest%2Fverysecret%2Fauthorize");
 		} catch (URISyntaxException e) {
@@ -59,43 +63,39 @@ public class VerySecretRestService extends AbstractRestService {
     }
     @GET
     @Path("/authorize")
-    public Response authorize(@QueryParam("code")String code,@QueryParam("state")String state,@Context Request request,@Context UriInfo uriInfo){
+    public Response authorize(@QueryParam("code")String code,@QueryParam("state")String state,@Context Request request,@Context UriInfo uriInfo) throws URISyntaxException{
     	
-    	URI uri = null;
-		String link = uriInfo.getPath().toString();
+    	URI uri = new URI("http://localhost:8080");
+		String link = uriInfo.getRequestUri().toString();
 		
     	
     	
     	
     	if(link.contains("error")){
     		//denied
-    		try {
-    			uri = new URI("http://localhost:8080");
-    		}
-    		catch (URISyntaxException e) {
-    			 
-    			e.printStackTrace();
-    		}
+    		
     		return Response.temporaryRedirect(uri).build();
     	}
     	
     		//request token
 
     	
-    	try {
-			uri = new URI("https://www.googleapis.com/oauth2/v4/token?"+
-    	"code=4/EDxem0UwFLAT2xY8eMeO9nmNO9z-f6rD7zlkxAj5g3o"+
-		"client_id=670282150821-0kd0pjsm09re6mtvjdm5s9ejek3hc4a0.apps.googleusercontent.com"+
-    	"client-secret=wEWpecZ0wi2Npw44WS8xceof"+
-		"redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Frest%2Fverysecret"+
-    	"grant_type=authorization_code");
-		
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-
+//    	try {
+//			uri = new URI("https://www.googleapis.com/oauth2/v4/token?"+
+//    	"code=4/EDxem0UwFLAT2xY8eMeO9nmNO9z-f6rD7zlkxAj5g3o"+
+//		"client_id=670282150821-0kd0pjsm09re6mtvjdm5s9ejek3hc4a0.apps.googleusercontent.com"+
+//    	"client-secret=wEWpecZ0wi2Npw44WS8xceof"+
+//		"redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Frest%2Fverysecret"+
+//    	"grant_type=authorization_code");
+//		
+//		} catch (URISyntaxException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//
+//    	return Response.temporaryRedirect(uri).build();
+    	uri = new URI("http://localhost:8080/rest/verysecret");
     	return Response.temporaryRedirect(uri).build();
 }
 
