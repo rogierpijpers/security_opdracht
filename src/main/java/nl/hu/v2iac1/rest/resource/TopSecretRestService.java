46144646a31558a5cc7bdcbb53b1e0ecf835e70a
spaghettimeester <nikkybruijn@gmail.com>
2016-02-22 22:19:15 +0100

    Iets verbeterd?

--- src/main/java/nl/hu/v2iac1/rest/resource/TopSecretRestService.java
+++ src/main/java/nl/hu/v2iac1/rest/resource/TopSecretRestService.java
@@ -7,61 +7,38 @@ import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import mail.SendMail;
+import nl.hu.v2iac1.Configuration;
 import auth.TokenGen;
 import auth.User;
 import auth.UserService;
-import mail.SendMail;
-import nl.hu.v2iac1.Configuration;
 
 @Path("/topsecret")
 @Produces(MediaType.TEXT_PLAIN)
 public class TopSecretRestService extends AbstractRestService {
 
-
-
 	@POST
-	@Path("/authenticate")
-	public Response authorize(@FormParam("username") String username, @FormParam("password") String password)
+	@Path("/authorized")
+	public Response getSecret(@FormParam("token") String token)
 			throws URISyntaxException {
-		UserService service = new UserService();
 
-		URI uri = new URI("http://localhost:8080/login.html");
+		URI uri = new URI("http://localhost:8080/rest/topsecret/");
 
-		if (service.authenticate(username, password)) {
-			TokenGen gen = TokenGen.getInstance();
-			String token = gen.generateToken();
+		TokenGen gen = TokenGen.getInstance();
 
-			User user = service.getUser(username);
-
-			SendMail.send(user, token);
-
+		if (!gen.cashToken(token)) {
 			return Response.temporaryRedirect(uri).build();
 		}
 
-		uri = new URI("http://localhost:8080");
-		return Response.temporaryRedirect(uri).build();
+		String output = "This is TOP secret: "
+				+ configuration.getValue(Configuration.Key.TOPSECRET);
+		return Response.status(200).entity(output).build();
+
 	}
-	@POST
-	 @Path("/{token}")
-	 public Response getSecret(@FormParam("token") String token) throws URISyntaxException {
-
-	  URI uri = new URI("http://localhost:8080/rest/topsecret/");
-	  
-	  TokenGen gen = TokenGen.getInstance();
-	  
-	  if (!gen.cashToken(token)) {  
-	   return Response.temporaryRedirect(uri).build();
-	  }
-	  
-	  String output = "This is very secret: " + configuration.getValue(Configuration.Key.VERYSECRET);
-	  return Response.status(200).entity(output).build();
-
-	 }
 
 	@GET
 	@Path("/")
@@ -74,7 +51,29 @@ public class TopSecretRestService extends AbstractRestService {
 			e.printStackTrace();
 		}
 		return Response.temporaryRedirect(uri).build();
+	}
 
-	}//hallo
+	@POST
+	@Path("/authenticate")
+	public Response authorize(@FormParam("username") String username,
+			@FormParam("password") String password) throws URISyntaxException {
+		UserService service = new UserService();
+
+		URI uri = new URI("http://localhost:8080/verification.html");
+
+		if (service.authenticate(username, password)) {
+			TokenGen gen = TokenGen.getInstance();
+			String token = gen.generateToken();
+
+			User user = service.getUser(username);
+
+			SendMail.send(user, token);
+
+			return Response.temporaryRedirect(uri).build();
+		}
+
+		uri = new URI("http://localhost:8080/login.html");
+		return Response.temporaryRedirect(uri).build();
+	}
 
 }
